010ad5e7a578099153284534910f8b5d145adaa5
Matthew Peak <matthewpeak@gmail.com>
2020-02-13 19:42:08 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
 # Your Code Here
+
+def map(array)
+i = 0 
+new_array = []
+  while i < array.length 
+  new_array[i] = yield(array[i])
+  i += 1
+  end
+return new_array
+end
+
+def reduce(array, starting_point = array[0])
+i = 0 
+value = starting_point
+if value == array[0]
+  array.shift()
+end
+  while i < array.length 
+  value = yield(value, array[i])
+  i += 1
+  end
+return value
+end
